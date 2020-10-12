1ede358dc6d84e86129641a124c0290788af3e26
Dorin <mirondorin99@gmail.com>
2020-10-12 14:36:33 +0300

    Added board & pen
    Drawing board & pen (no vr controller support yet)

---
+++ Assets/Scripts/Pen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Pen : VRTK_InteractableObject
+{
+    public DrawingBoard drawingBoard;
+    private RaycastHit touch;
+    private bool lastTouch;
+    private Quaternion lastAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.drawingBoard = GameObject.Find("DrawingBoard").GetComponent<DrawingBoard>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tipHeight = transform.Find("Tip").transform.localScale.y;
+        Vector3 tip = transform.Find("Tip").transform.position;
+
+        if (Physics.Raycast(tip, transform.up, out touch, tipHeight))
+        {
+            if (touch.collider.tag != "DrawingBoard") return;
+
+            this.drawingBoard = touch.collider.GetComponent<DrawingBoard>();
+
+            this.drawingBoard.SetColor(Color.blue);
+            this.drawingBoard.SetTouchPosition(touch.textureCoord.x, touch.textureCoord.y);
+            this.drawingBoard.ToggleTouch(true);
+
+            if (!lastTouch)
+            {
+                lastTouch = true;
+                lastAngle = transform.rotation;
+            }
+        }
+
+        else
+        {
+            this.drawingBoard.ToggleTouch(false);
+            lastTouch = false;
+        }
+
+        if(lastTouch)
+        {
+            transform.rotation = lastAngle;
+            tipHeight *= 1.1f;
+        }
+    }
+}
